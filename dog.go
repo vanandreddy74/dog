4d368c02e2efffc8824a958b08cf39916c312acd
vanandhpe <var@hpe.com>
2023-06-26 23:30:27 +0530

    second

---
+++ dog.go
@@ -0,0 +1,9 @@
+package dog
+
+import (
+	"strings"
+)
+
+func WhenGrownup(s string) string {
+	return "when the puppy grows up it says: " + strings.ToUpper(s)
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/vanandreddy74/dog
+
+go 1.17
